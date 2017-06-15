7bfc244e62a2517242373a5b3cc026999cb1dbec
liamzebedee <liamzebedee@yahoo.com.au>
2017-06-15 17:01:29 +1000

    allow

--- backend/settings.py
+++ backend/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '0.0.0.0']
+ALLOWED_HOSTS = ['localhost', '0.0.0.0', 'www.sydneysunsets.com']
 
 
 
